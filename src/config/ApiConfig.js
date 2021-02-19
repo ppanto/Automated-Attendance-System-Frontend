e3b5f15e7bc628eca2ed4f964adbb725909b9bc1
adleto <52221117+yukiteru.eu@gmail.com>
2021-02-19 01:08:30 +0100

    Changed api path for docker

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,2 +1,4 @@
-export const BASE_PATH = 'http://192.168.137.1:8080/api';
+export const BASE_PATH = '/api'
+//export const BASE_PATH = 'http://app:8080/api'
+//export const BASE_PATH = 'http://192.168.137.1:8080/api';
 //export const BASE_PATH = 'http://localhost:8080/api';
